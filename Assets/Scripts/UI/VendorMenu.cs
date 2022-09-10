3107dab6b8853ae8c541f6b3d9ebaaa9ac80b054
ajvark <ajvark@gmail.com>
2022-09-10 17:58:50 -0400

    further implementation of the shop menu

--- Assets/Scripts/UI/VendorMenu.cs
+++ Assets/Scripts/UI/VendorMenu.cs
@@ -1,29 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+using UnityEngine.EventSystems;
 
-
-public class VendorObject : MonoBehaviour
+public class VendorMenu : MonoBehaviour
 {
 
     public List<VendorItem> Items;
-    
+
     public List<VendorItem> order;
 
-//    public Dialogue boughtsomething, boughtnothing, ordercheck;
+    //    public Dialogue boughtsomething, boughtnothing, ordercheck;
+
+    public void Update()
+    {
+        /*if (IsPointerOverUIElement())
+        {
+
+        }*/
+    }
+
+    ///Returns 'true' if we touched or hovering on Unity UI element.
+    public static bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+    ///Returns 'true' if we touched or hovering on Unity UI element.
+    public static bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == LayerMask.NameToLayer("UI"))
+            {
+                Debug.Log(curRaysastResult.gameObject.name);
+                return true;
+            }
+        }
+        return false;
+    }
+    ///Gets all event systen raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
 
 
     public void AddToOrder(VendorItem item)
     {
         order.Add(item);
         Debug.Log(string.Format("added {0} to order", item.name));
-        FindObjectOfType<VendorHelper>().RefreshOrder();
+        UpdateQuantity();
     }
 
     public void RemoveFromOrder(VendorItem item)
     {
-        order.Remove(item);
-        FindObjectOfType<VendorHelper>().RefreshOrder();
+        if (order.Contains(item))
+            order.Remove(item);
+        UpdateQuantity();
+    }
+
+    public void UpdateQuantity()
+    {
+        for(int i = 0; i < Items.Count; i++)
+        {
+            transform.GetChild(i).GetChild(2).GetComponent<TMP_InputField>().text = order.Where(x => x == Items[i]).Count().ToString();
+        }
     }
 
     public void Checkout()
