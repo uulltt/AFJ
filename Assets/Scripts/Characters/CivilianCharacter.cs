230bbd99ad534ea89cc538c9ef7e333f4ac70a66
BrettMagruder <Brett.Magruder@proactivetechinc.com>
2022-09-10 22:59:20 -0400

    THE DUDES! THEY GAMIN

--- Assets/Scripts/Characters/CivilianCharacter.cs
+++ Assets/Scripts/Characters/CivilianCharacter.cs
@@ -4,4 +4,27 @@ using UnityEngine;
 
 public class CivilianCharacter : AbstractCharacter
 {
+    AreaOfInterest currentDestination;
+
+    float DeadZone = 1f;
+
+    Timer wanderTimer = new Timer(3);
+
+    private void Update()
+    {
+        if(currentDestination == null && AreaOfInterest.areasOfInterest.Count > 0 && wanderTimer.elapseAsPercent >= 1)
+        {
+            currentDestination = AreaOfInterest.areasOfInterest[Random.Range(0, AreaOfInterest.areasOfInterest.Count)];
+            locomotor.MoveTowardsSpot(currentDestination.transform.position);
+        }
+        else if(Vector3.Distance(transform.position, currentDestination.transform.position) < DeadZone)
+        {
+            wanderTimer.Reset();
+            wanderTimer.maxTime = Random.Range(1f, 10f);
+
+            currentDestination = null;
+        }
+    }
+
+
 }
