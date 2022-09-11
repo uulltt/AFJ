3c6172bdc9cb45a983f2fc61db28569a86dcecc8
BrettMagruder <Brett.Magruder@proactivetechinc.com>
2022-09-11 13:52:05 -0400

    Guard Characters now fire on hostiles

--- Assets/Scripts/Characters/GuardCharacter.cs
+++ Assets/Scripts/Characters/GuardCharacter.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public class GuardCharacter : AbstractCharacter
 {    public AbstractCharacter currentTarget;    public override void ReactToCharacter(AbstractCharacter whoReactTo)
     {
-        if (whoReactTo == currentTarget)
-            Attack(whoReactTo);
+        Attack(whoReactTo);
     }}
